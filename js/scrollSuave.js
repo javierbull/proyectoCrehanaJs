ed359411784ddd8c82599f3f2d7db13e2d35a32f
Javier Bull <javierbullreyes@gmail.com>
2020-08-18 18:16:31 -0500

    modal con js

--- js/scrollSuave.js
+++ js/scrollSuave.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
 
-  alert('Hola');
+  // Mostrar y ocultar btn volver arriba
+
+  var btnVolverArriba = $('#btnVolverArriba');
+
+  $(window).on('scroll', function(){
+
+    var proyectosOffSetTop = $('#proyectos').offset().top;
+
+    if ( $(window).scrollTop() >= proyectosOffSetTop ) {
+      btnVolverArriba.css('margin-right', 0);
+    } else if ($(window).scrollTop() <= proyectosOffSetTop ) {
+      btnVolverArriba.css('margin-right', '-60px');
+    }
+
+  })
+
+  // Scroll suave
+
+  // $('a.volver-arriba').on('click', function(e){
+  //   e.preventDefault();
+  //
+  //   if ($(window).scrollTop() != 0) {
+  //     $('html, body').animate({scrollTop:0}, 1000)
+  //   }
+  // });
+
 });
