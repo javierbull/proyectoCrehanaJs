025a7b1486e6fbedc2c75940f1ae38835bc2c3d6
Javier Bull <javierbullreyes@gmail.com>
2020-08-19 12:06:18 -0500

    slider automático

--- js/slider.js
+++ js/slider.js
@@ -12,12 +12,16 @@ $(document).ready(function(){
 
   //Función para q el slide se mueva a la derecha
   function moverderecha(){
-    slider.animate({
-      marginLeft: '-105.6%'
-    }, 700, function(){
-      $('#slider .slide:first').insertAfter('#slider .slide:last');
-      slider.css('margin-left', '-43%');
-    });
+
+    if (!slider.is(':animated')) {
+      slider.animate({
+        marginLeft: '-105.6%'
+      }, 700, function(){
+        $('#slider .slide:first').insertAfter('#slider .slide:last');
+        slider.css('margin-left', '-43%');
+        resetInterval();
+      });
+    }
   }
 
   btnSiguiente.on('click', moverderecha);
@@ -25,13 +29,26 @@ $(document).ready(function(){
   // Función para mover el slide a la izquierda
 
   function moverIzquierda(){
-    $('#slider .slide:last').insertBefore('#slider .slide:first');
-    slider.css('margin-left', '-105.6%');
-    slider.animate({
-      marginLeft: '-43%'
-    }, 700);
+    if (!slider.is(':animated')) {
+      $('#slider .slide:last').insertBefore('#slider .slide:first');
+      slider.css('margin-left', '-105.6%');
+      slider.animate({
+        marginLeft: '-43%'
+      }, 700, function(){
+        resetInterval()
+      });
+    }
+
   }
 
   btnAnterior.on('click', moverIzquierda);
+
+  // Intervalo para que el slide se mueva automáticamente
+  var intervalo = setInterval(moverderecha, 5000);
+
+  function resetInterval(){
+    clearInterval(intervalo);
+    intervalo = setInterval(moverderecha, 5000);
+  }
 
 });
